8c5984ae73e580732c95413efac3fed368b71e47
Seena Rowhani <seenarowhani95@gmail.com>
2018-04-09 00:09:03 -0400

    test(pso): add test cases for property sort order

--- test/src/resolvers/property-sort-order.ts
+++ test/src/resolvers/property-sort-order.ts
@@ -1,6 +1,64 @@
 import resolve, { detect, lint } from '@test/helpers/resolve';
 
 describe('property-sort-order', () => {
+  describe('- scss', () => {
+    describe('- alphabetical', () => {
+      const options = { 'property-sort-order': 1 };
+      it('resolves', done => {
+        const filename = 'test/sass/property-sort-order.scss';
+        resolve(filename, { ...options }, (_, __, resolvedTree) => {
+          const preResolve = lint(filename, options);
+          const postResolve = detect(resolvedTree.toString(), 'scss', options);
+          expect(preResolve.warningCount).toBe(19);
+          expect(postResolve.warningCount).toBe(0);
+          done();
+        });
+      });
+    });
+
+    describe('- smacss', () => {
+      const options = {
+        'property-sort-order': [
+          1,
+          {
+            order: 'smacss',
+          },
+        ],
+      };
+      it('resolves', done => {
+        const filename = 'test/sass/property-sort-order.scss';
+        resolve(filename, { ...options }, (_, __, resolvedTree) => {
+          const preResolve = lint(filename, options);
+          const postResolve = detect(resolvedTree.toString(), 'scss', options);
+          expect(preResolve.warningCount).toBe(12);
+          expect(postResolve.warningCount).toBe(0);
+          done();
+        });
+      });
+    });
+
+    describe('- recess', () => {
+      const options = {
+        'property-sort-order': [
+          1,
+          {
+            order: 'recess',
+          },
+        ],
+      };
+      it('resolves', done => {
+        const filename = 'test/sass/property-sort-order.scss';
+        resolve(filename, { ...options }, (_, __, resolvedTree) => {
+          const preResolve = lint(filename, options);
+          const postResolve = detect(resolvedTree.toString(), 'scss', options);
+          expect(preResolve.warningCount).toBe(12);
+          expect(postResolve.warningCount).toBe(0);
+          done();
+        });
+      });
+    });
+  });
+
   describe('- sass', () => {
     describe('- alphabetical', () => {
       const options = { 'property-sort-order': 1 };
@@ -15,62 +73,47 @@ describe('property-sort-order', () => {
         });
       });
     });
-  });
-  describe('- scss', () => {
-    describe('- alphabetical', () => {
-      const options = { 'property-sort-order': 1 };
+
+    describe('- smacss', () => {
+      const options = {
+        'property-sort-order': [
+          1,
+          {
+            order: 'smacss',
+          },
+        ],
+      };
       it('resolves', done => {
-        const filename = 'test/sass/property-sort-order.scss';
+        const filename = 'test/sass/property-sort-order.sass';
         resolve(filename, { ...options }, (_, __, resolvedTree) => {
           const preResolve = lint(filename, options);
-          const postResolve = detect(resolvedTree.toString(), 'scss', options);
-          expect(preResolve.warningCount).toBe(15);
+          const postResolve = detect(resolvedTree.toString(), 'sass', options);
+          expect(preResolve.warningCount).toBe(16);
           expect(postResolve.warningCount).toBe(0);
           done();
         });
       });
     });
 
-    describe('- smacss', () => {
-      // const options = {
-      //   'property-sort-order': [
-      //     1,
-      //     {
-      //       order: 'smacss',
-      //     },
-      //   ],
-      // };
-      // it('resolves', done => {
-      //   const filename = 'test/sass/property-sort-order.scss';
-      //   resolve(filename, {...options}, (_, __, resolvedTree) => {
-      //     const preResolve = lint(filename, options);
-      //     const postResolve = detect(resolvedTree.toString(), 'scss', options);
-      //     expect(preResolve.warningCount).toBe(12);
-      //     expect(postResolve.warningCount).toBe(0);
-      //     done();
-      //   });
-      // });
-    });
-
     describe('- recess', () => {
-      // const options = {
-      //   'property-sort-order': [
-      //     1,
-      //     {
-      //       order: 'recess',
-      //     },
-      //   ],
-      // };
-      // it('resolves', done => {
-      //   const filename = 'test/sass/property-sort-order.scss';
-      //   resolve(filename, {...options}, (_, __, resolvedTree) => {
-      //     const preResolve = lint(filename, options);
-      //     const postResolve = detect(resolvedTree.toString(), 'scss', options);
-      //     expect(preResolve.warningCount).toBe(12);
-      //     expect(postResolve.warningCount).toBe(0);
-      //     done();
-      //   });
-      // });
+      const options = {
+        'property-sort-order': [
+          1,
+          {
+            order: 'recess',
+          },
+        ],
+      };
+      it('resolves', done => {
+        const filename = 'test/sass/property-sort-order.sass';
+        resolve(filename, { ...options }, (_, __, resolvedTree) => {
+          const preResolve = lint(filename, options);
+          const postResolve = detect(resolvedTree.toString(), 'sass', options);
+          expect(preResolve.warningCount).toBe(16);
+          expect(postResolve.warningCount).toBe(0);
+          done();
+        });
+      });
     });
   });
 });
