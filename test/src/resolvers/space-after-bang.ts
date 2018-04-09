e89574205a5f526466f92ce17d0299fe9cebabb7
Seena Rowhani <seenarowhani95@gmail.com>
2018-04-09 15:05:10 -0400

    test(resolver): space-after-colon

--- test/src/resolvers/space-after-bang.ts
+++ test/src/resolvers/space-after-bang.ts
@@ -1,40 +1,85 @@
 import resolve, { detect, lint } from '@test/helpers/resolve';
 
 describe('space-after-bang', () => {
-  const options = { 'space-after-bang': 1 };
-
   describe('scss', () => {
-    it('resolves', done => {
-      const filename = 'test/sass/space-after-bang.scss';
-      resolve(filename, options, (_, __, resolvedTree) => {
-        const preResolve = lint(filename, {
-          'space-after-bang': 1,
-        });
-        const postResolve = detect(resolvedTree.toString(), 'scss', {
-          'space-after-bang': 1,
-        });
+    describe('[include: false]', () => {
+      const options = {
+        'space-after-bang': [
+          1,
+          {
+            include: false,
+          },
+        ],
+      };
+      it('resolves', done => {
+        const filename = 'test/sass/space-after-bang.scss';
+        resolve(filename, options, (_, __, resolvedTree) => {
+          const preResolve = lint(filename, options);
+          const postResolve = detect(resolvedTree.toString(), 'scss', options);
 
-        expect(preResolve.warningCount).toBe(7);
-        expect(postResolve.warningCount).toBe(0);
-        done();
+          expect(preResolve.warningCount).toBe(7);
+          expect(postResolve.warningCount).toBe(0);
+          done();
+        });
+      });
+    });
+    describe('[include: true]', () => {
+      const options = {
+        'space-after-bang': [
+          1,
+          {
+            include: true,
+          },
+        ],
+      };
+      it('resolves', done => {
+        const filename = 'test/sass/space-after-bang.scss';
+        resolve(filename, { ...options }, (_, __, resolvedTree) => {
+          const preResolve = lint(filename, options);
+          const postResolve = detect(resolvedTree.toString(), 'scss', options);
+          expect(preResolve.warningCount).toBe(7);
+          // TODO: sass-lint reports errors on include space
+          expect(postResolve.warningCount).toBe(14);
+          done();
+        });
       });
     });
   });
 
   describe('sass', () => {
-    it('resolves', done => {
-      const filename = 'test/sass/space-after-bang.sass';
-      resolve(filename, options, (_, __, resolvedTree) => {
-        const preResolve = lint(filename, {
-          'space-after-bang': 1,
-        });
-        const postResolve = detect(resolvedTree.toString(), 'sass', {
-          'space-after-bang': 1,
-        });
+    describe('[include: false]', () => {
+      const options = { 'space-after-bang': 1 };
+      it('resolves', done => {
+        const filename = 'test/sass/space-after-bang.sass';
+        resolve(filename, options, (_, __, resolvedTree) => {
+          const preResolve = lint(filename, options);
+          const postResolve = detect(resolvedTree.toString(), 'sass', options);
 
-        expect(preResolve.warningCount).toBe(7);
-        expect(postResolve.warningCount).toBe(0);
-        done();
+          expect(preResolve.warningCount).toBe(7);
+          expect(postResolve.warningCount).toBe(0);
+          done();
+        });
+      });
+    });
+    describe('[include: true]', () => {
+      const options = {
+        'space-after-bang': [
+          1,
+          {
+            include: true,
+          },
+        ],
+      };
+      it('resolves', done => {
+        const filename = 'test/sass/space-after-bang.sass';
+        resolve(filename, { ...options }, (_, __, resolvedTree) => {
+          const preResolve = lint(filename, options);
+          const postResolve = detect(resolvedTree.toString(), 'sass', options);
+          expect(preResolve.warningCount).toBe(7);
+          // TODO: sass-lint reports errors on include space
+          expect(postResolve.warningCount).toBe(14);
+          done();
+        });
       });
     });
   });
