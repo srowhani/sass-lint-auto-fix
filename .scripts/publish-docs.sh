63be6d438c30773a75c6f77d7f0dd6d515613a1b
Seena Rowhani <seenarowhani95@gmail.com>
2018-04-11 13:16:48 -0400

    docs(publish): adjust deployment script

--- .scripts/publish-docs.sh
+++ .scripts/publish-docs.sh
@@ -2,12 +2,13 @@ REMOTE=$(git config --get remote.origin.url);
 VERSION=$(node -e "console.log(require('./package.json').version)");
 
 cd docs;
+rm -rf .git;
 git init;
 git remote add origin $REMOTE;
 git checkout -b gh-pages;
 git add -A .;
-git commit -m 'docs($VERSION)';
+git commit -m "docs($VERSION)";
 git push origin gh-pages -f;
+COMMIT_HASH=$(git rev-parse --verify HEAD);
+echo "Published: $COMMIT_HASH!";
 cd ..;
-
-echo 'Published @$(git rev-parse --verify HEAD)!'
