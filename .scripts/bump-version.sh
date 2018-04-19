956b750c5d08c46f764a5c9d27cc225572ce10d7
Seena Rowhani <seenarowhani95@gmail.com>
2018-04-18 23:48:43 -0400

    refactor(scripts): remove deploy scripts

--- .scripts/bump-version.sh
+++
@@ -1,8 +0,0 @@
-VERSION=$(node -e "console.log(require('./package.json').version)");
-
-git add -A .;
-git commit -m "build($VERSION): v$VERSION";
-git tag "v$VERSION";
-
-git push origin master;
-git push origin --tags;
