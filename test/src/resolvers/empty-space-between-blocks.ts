9a9cec6694ef97c789348b42bb7ab3455229f0dd
Seena Rowhani <seenarowhani95@gmail.com>
2018-04-23 20:10:23 -0400

    refactor(fix): only attempt scss files

---
+++ test/src/resolvers/empty-space-between-blocks.ts
@@ -0,0 +1,20 @@
+import resolve, { detect, lint } from '@test/helpers/resolve';
+
+describe('empty-space-between-blocks', () => {
+  describe('scss', () => {
+    describe('[include: true, allow-single-line-rulesets: true]', () => {
+      const options = { 'empty-line-between-blocks': 1 };
+      it('resolves', done => {
+        const filename = 'test/sass/empty-line-between-blocks.scss';
+        resolve(filename, options, (_, __, resolvedTree) => {
+          const preResolve = lint(filename, options);
+          const postResolve = detect(resolvedTree.toString(), 'scss', options);
+
+          expect(preResolve.warningCount).toBe(3);
+          expect(postResolve.warningCount).toBe(0);
+          done();
+        });
+      });
+    });
+  });
+});
