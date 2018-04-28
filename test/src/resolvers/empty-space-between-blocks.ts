2c233744b446172cc3318bf2383d3b266da1d217
Seena Rowhani <seenarowhani95@gmail.com>
2018-04-27 20:12:48 -0400

    test(resolver): assert elbb doesnt modify sass

--- test/src/resolvers/empty-space-between-blocks.ts
+++ test/src/resolvers/empty-space-between-blocks.ts
@@ -1,5 +1,7 @@
 import resolve, { detect, lint } from '@test/helpers/resolve';
 
+const fs = require('fs');
+
 describe('empty-space-between-blocks', () => {
   describe('scss', () => {
     describe('[include: true, allow-single-line-rulesets: true]', () => {
@@ -14,6 +16,19 @@ describe('empty-space-between-blocks', () => {
           expect(postResolve.warningCount).toBe(1);
           done();
         });
+      });
+    });
+  });
+  describe('sass', () => {
+    const options = { 'empty-line-between-blocks': 1 };
+
+    it('does not modify', done => {
+      const filename = 'test/sass/empty-line-between-blocks.sass';
+      resolve(filename, options, (_, __, resolvedTree) => {
+        expect(fs.readFileSync(filename).toString()).toBe(
+          resolvedTree.toString(),
+        );
+        done();
       });
     });
   });
