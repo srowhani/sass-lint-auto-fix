eed31336a9df4c32ea5368b09eff1e3e61379701
Seena Rowhani <seenarowhani95@gmail.com>
2018-09-30 18:35:35 -0400

    fix(resolver): empty-line-between-blocks

--- src/resolvers/empty-line-between-blocks.ts
+++ src/resolvers/empty-line-between-blocks.ts
@@ -1,7 +1,16 @@
-import { AbstractSyntaxTree, SlRule } from '@src/typings';
+import { AbstractSyntaxTree, SlRule, Nullable } from '@src/typings';
 import BaseResolver from './base-resolver';
 
 const gonzales = require('gonzales-pe-sl');
+
+enum TokenType {
+  NEWLINE = '\n',
+  OPEN = '{',
+  CLOSE = '}',
+  EMPTY = '',
+}
+
+type Block = { type: TokenType, lineNumber: number };
 
 export default class EmptyLineBetweenBlocks extends BaseResolver {
   private _scssEmptyLineRegex: RegExp;
@@ -15,11 +24,47 @@ export default class EmptyLineBetweenBlocks extends BaseResolver {
     const { ast } = this;
     // TODO: Implement `fix` for sass
     if (ast.syntax === 'scss') {
-      if (this.shouldInjectNewline()) {
+      if (this.canInjectNewline()) {
         const content = ast.toString();
+        const splitContent = content.split('\n');
+        const blocks: Block[] = [];
 
-        const newContent = this.sanitize(content);
-        const newTree = gonzales.parse(newContent, {
+        const types = [TokenType.CLOSE, TokenType.OPEN];
+
+        splitContent.forEach((line, lineNumber) => {
+          types.forEach(type => {
+            if (line.includes(type)) {
+              blocks.push({
+                type,
+                lineNumber: lineNumber + 1,
+              });
+            }
+          });
+          blocks.push({
+            type: TokenType.NEWLINE,
+            lineNumber: lineNumber + 1,
+          });
+        });
+
+        const injectableBlocks = blocks.filter((block, index) => {
+          if (block.type === TokenType.CLOSE) {
+            if (this.shouldInjectNewline(blocks.slice(index + 1))) {
+              return true;
+            }
+          }
+          return false;
+        });
+
+        let numInjected = 0;
+
+        injectableBlocks.forEach(
+          ({ lineNumber }) => splitContent.splice(
+            lineNumber + numInjected++,
+            0,
+            TokenType.EMPTY,
+          )
+        );
+        const newTree = gonzales.parse(splitContent.join(TokenType.NEWLINE), {
           syntax: 'scss',
         });
 
@@ -29,7 +74,19 @@ export default class EmptyLineBetweenBlocks extends BaseResolver {
     return ast;
   }
 
-  private shouldInjectNewline(): boolean {
+  private shouldInjectNewline (blocks: Block[]) {
+    let c = 0;
+    for (const block of blocks) {
+      if (block.type === TokenType.NEWLINE) {
+        c++;
+      } else if (block.type === TokenType.OPEN) {
+        return c < 2;
+      }
+    }
+    return false;
+  }
+
+  private canInjectNewline(): boolean {
     return (
       this.parser.options.include === true &&
       this.parser.options['allow-single-line-rulesets'] === true
