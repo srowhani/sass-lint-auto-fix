7c16902b94d8e8ca2fe88d2cc0efa66397750a3d
Seena Rowhani <srowhani@srowhani-macbookpro.roam.corp.google.com>
2019-02-17 16:21:27 -0800

    fix(property-sort-order): correctly arrange lines when sorting props

--- src/typings/abstract-syntax-tree.ts
+++ src/typings/abstract-syntax-tree.ts
@@ -23,14 +23,89 @@ export interface AbstractSyntaxTree {
   traverseByType(nodeType: string, callback: traversalCallback): void;
   traverseByTypes(nodeTypes: string[], callback: traversalCallback): void;
   removeChild(index: number): TreeNode;
+  toString(): string;
+}
+
+export enum NodeType {
+  arguments = 'arguments',
+  atkeyword = 'atkeyword',
+  atrule = 'atrule',
+  attributeFlags = 'attributeFlags',
+  attributeMatch = 'attributeMatch',
+  attributeName = 'attributeName',
+  attributeSelector = 'attributeSelector',
+  attributeValue = 'attributeValue',
+  block = 'block',
+  brackets = 'brackets',
+  class = 'class',
+  color = 'color',
+  combinator = 'combinator',
+  condition = 'condition',
+  conditionalStatement = 'conditionalStatement',
+  declaration = 'declaration',
+  declarationDelimiter = 'declarationDelimiter',
+  default = 'default',
+  delimiter = 'delimiter',
+  dimension = 'dimension',
+  escapedString = 'escapedString',
+  extend = 'extend',
+  expression = 'expression',
+  function = 'function',
+  global = 'global',
+  id = 'id',
+  ident = 'ident',
+  important = 'important',
+  include = 'include',
+  interpolatedVariable = 'interpolatedVariable',
+  interpolation = 'interpolation',
+  keyframesSelector = 'keyframesSelector',
+  loop = 'loop',
+  mixin = 'mixin',
+  multilineComment = 'multilineComment',
+  namePrefix = 'namePrefix',
+  namespacePrefix = 'namespacePrefix',
+  namespaceSeparator = 'namespaceSeparator',
+  number = 'number',
+  operator = 'operator',
+  parentheses = 'parentheses',
+  parentSelector = 'parentSelector',
+  parentSelectorExtension = 'parentSelectorExtension',
+  percentage = 'percentage',
+  placeholder = 'placeholder',
+  progid = 'progid',
+  property = 'property',
+  propertyDelimiter = 'propertyDelimiter',
+  pseudoClass = 'pseudoClass',
+  pseudoElement = 'pseudoElement',
+  raw = 'raw',
+  ruleset = 'ruleset',
+  space = 'space',
+  selector = 'selector',
+  singlelineComment = 'singlelineComment',
+  string = 'string',
+  stylesheet = 'stylesheet',
+  typeSelector = 'typeSelector',
+  unicodeRange = 'unicodeRange',
+  universalSelector = 'universalSelector',
+  urange = 'urange',
+  uri = 'uri',
+  value = 'value',
+  variable = 'variable',
+  variablesList = 'variablesList',
+}
+
+export interface NodePosition {
+  line: number;
+  column: number;
 }
 
 export interface TreeNode extends AbstractSyntaxTree {
-  type: string;
+  type: NodeType;
+  start: NodePosition;
+  end: NodePosition;
   forEach(nodeType: string, callback: traversalCallback): void;
   first(nodeType?: string): Nullable<TreeNode>;
   last(nodeType?: string): Nullable<TreeNode>;
-  toString(): string;
 }
 
 export interface SortNode {
--- test/src/resolvers/property-sort-order.ts
+++ test/src/resolvers/property-sort-order.ts
@@ -1,19 +1,47 @@
 import { ValidFileType } from '@src/typings';
 import { detect, lint, resolveFirst } from '@test/helpers/resolve';
 
+import { dedent } from '@test/helpers/utils';
+import fs from 'fs';
+
 describe('property-sort-order', () => {
   describe('- scss', () => {
     describe('- alphabetical', () => {
       const options = { 'property-sort-order': 1 };
+      const filename = 'test/sass/property-sort-order.scss';
+
       it('resolves', () => {
-        const filename = 'test/sass/property-sort-order.scss';
         const { ast } = resolveFirst(filename, options);
 
         const preResolve = lint(filename, options);
         const postResolve = detect(ast.toString(), ValidFileType.scss, options);
 
-        expect(preResolve.warningCount).toBe(19);
+        expect(preResolve.warningCount).toBe(21);
         expect(postResolve.warningCount).toBe(0);
+      });
+
+      it('preserves sass-lint disable-line properly', () => {
+        const preResolve = fs.readFileSync(filename).toString();
+
+        expect(preResolve).toContain(
+          dedent(`
+          .disable-line-test {
+            display: block;
+            border: none; // sass-lint:disable-line border-zero
+          }
+        `),
+        );
+
+        const { ast: postResolve } = resolveFirst(filename, options, true);
+
+        expect(postResolve.toString()).toContain(
+          dedent(`
+          .disable-line-test {
+            border: none; // sass-lint:disable-line border-zero
+            display: block;
+          }
+        `),
+        );
       });
     });
 
@@ -84,16 +112,38 @@ describe('property-sort-order', () => {
 
   describe('- sass', () => {
     describe('- alphabetical', () => {
+      const filename = 'test/sass/property-sort-order.sass';
       const options = { 'property-sort-order': 1 };
-      it('resolves', () => {
-        const filename = 'test/sass/property-sort-order.sass';
 
+      it('resolves', () => {
         const { ast } = resolveFirst(filename, options);
 
         const preResolve = lint(filename, options);
         const postResolve = detect(ast.toString(), ValidFileType.sass, options);
-        expect(preResolve.warningCount).toBe(15);
+        expect(preResolve.warningCount).toBe(17);
         expect(postResolve.warningCount).toBe(0);
+      });
+
+      it('preserves sass-lint disable-line properly', () => {
+        const preResolve = fs.readFileSync(filename).toString();
+
+        expect(preResolve).toContain(
+          dedent(`
+          .disable-line-test
+            display: block
+            border: none // sass-lint:disable-line border-zero
+        `),
+        );
+
+        const { ast: postResolve } = resolveFirst(filename, options, true);
+
+        expect(postResolve.toString()).toContain(
+          dedent(`
+          .disable-line-test
+            border: none // sass-lint:disable-line border-zero
+            display: block
+        `),
+        );
       });
     });
 
